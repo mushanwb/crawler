d09fc5650ae79b6908fb0ff4a5d36410e7b94046
wangbo <1216476406@qq.com>
2020-08-10 17:42:42 +0800

    修改 CI 配置 使之能够再 CircleCI 上运行

--- src/main/java/com/github/mushanwb/Main.java
+++ src/main/java/com/github/mushanwb/Main.java
@@ -1,8 +1,26 @@
 package com.github.mushanwb;
-import java.util.*;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+
+        HttpGet httpGet = new HttpGet("https://sina.cn/");
+
+        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            System.out.println(EntityUtils.toString(entity1));
+
+        }
     }
 }
---
+++ src/test/java/com/github/mushanwb/SmokeTest.java
@@ -0,0 +1,10 @@
+package com.github.mushanwb;
+
+import org.junit.jupiter.api.Test;
+
+public class SmokeTest {
+    @Test
+    public void test() {
+        
+    }
+}
