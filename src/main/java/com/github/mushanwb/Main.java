2234153a8183bcc29690ac230b738ae0dae4969d
mushan <49012328+mushanwb@users.noreply.github.com>
2020-08-18 15:21:15 +0800

    引入 h2 数据库,并且简单优化代码 (#2)
    在这个提交中,引入了 h2 数据库代替连接池,
    并且将很长的 main 方法做了一个简单的优化操作.
    由于在代码中暴露了数据库的账号密码,使得 spotbugs 检查不通过,
    因此添加了 findbugs ,使用注解来避过这个不通过,
    当然这并不是一个好的行为,但目前我暂时这么做

--- src/main/java/com/github/mushanwb/Main.java
+++ src/main/java/com/github/mushanwb/Main.java
@@ -1,5 +1,6 @@
 package com.github.mushanwb;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -9,51 +10,102 @@ import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-
+import java.io.File;
 import java.io.IOException;
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    private static final String DATABASE_USER_NAME = "root";
+    private static final String DATABASE_USER_PASSWORD = "root123";
 
-        List<String> linkPool = new ArrayList<>();
-        Set<String> processedLinks = new HashSet<>();
+    @SuppressFBWarnings("DMI_CONSTANT_DB_PASSWORD")
+    public static void main(String[] args) throws IOException, SQLException {
 
-        linkPool.add("https://sina.cn");
+        File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
+        String jdbcUrl = "jdbc:h2:file:" + new File(projectDir, "news").getAbsolutePath();
+        Connection connection = DriverManager.getConnection(jdbcUrl, DATABASE_USER_NAME, DATABASE_USER_PASSWORD);
 
-        while (true) {
-            if (linkPool.isEmpty()) {
-                break;
-            }
+        String link;
+        // 从数据库中取出要被解析的链接并且删除，如果取出的为null，则表示没有要解析的链接
+        while ((link = getNextLinkThenDelete(connection)) != null) {
 
-            String link = linkPool.remove(linkPool.size()-1);
-            System.out.println(link);
-
-            if (processedLinks.contains(link)) {
+            // 链接已经被解析过了
+            if (isLinkProcessed(connection, link)) {
                 continue;
             }
 
+            // 如果链接时有效链接，则进行解析，有效链接即为分析得出来得链接规则
             if (isInterestingLink(link)) {
+                // 获取链接的 html 文档
                 Document doc = httpGetAndParseHtml(link);
 
-                ArrayList<Element> links = doc.select("a");
-                for (Element aTag:links) {
-                    if (isNesPage(aTag.attr("href"))) {
-                        linkPool.add(aTag.attr("href"));
-                    }
-                }
-//                doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkPool::add);
+                // 分析 html 文档并且从中获取新的新闻链接放入到即将处理链接的数据库中
+                parseUrlsFromPageAndStoreIntoDatabase(connection, doc);
 
+                // 分析文档，如果时新闻，则插入到数据库中（有标题的则为新闻）
                 storeIntoDatabaseIfItIsNewsPage(doc);
 
-                processedLinks.add(link);
-            } else {
+                // 处理完的链接加入到已处理的数据库中
+                updateDatabase(connection, link, "INSERT INTO LINKS_ALREADY_PROCESSED (link) VALUES (?)");
 
             }
         }
+    }
+
+    private static void parseUrlsFromPageAndStoreIntoDatabase(Connection connection, Document doc) throws SQLException {
+        ArrayList<Element> links = doc.select("a");
+        for (Element aTag:links) {
+            String href = aTag.attr("href");
+            if (isNesPage(href)) {
+                // 将爬取的链接放入即将处理的数据中
+                updateDatabase(connection, href, "INSERT INTO LINKS_TO_BE_PROCESSED (link) VALUES (?)");
+            }
+        }
+        //doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkPool::add);
+    }
+
+    // 从数据库加载已经处理的链接
+    private static boolean isLinkProcessed(Connection connection, String link) throws SQLException {
+        ResultSet resultSet = null;
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM LINKS_ALREADY_PROCESSED WHERE LINK = ?")) {
+            statement.setString(1, link);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                return true;
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+        return false;
+    }
+
+    private static void updateDatabase(Connection connection, String href, String sql) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, href);
+            statement.executeUpdate();
+        }
+    }
+
+    private static String getNextLink(Connection connection, String sql) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(sql); ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                return resultSet.getNString("LINK");
+            }
+        }
+        return null;
+    }
+
+    private static String getNextLinkThenDelete(Connection connection) throws SQLException {
+        String link = getNextLink(connection, "SELECT * FROM LINKS_TO_BE_PROCESSED LIMIT 1");
+        System.out.println(link);
+        if (link != null) {
+            updateDatabase(connection, link, "DELETE FROM LINKS_TO_BE_PROCESSED WHERE link = ?");
+        }
+        return link;
     }
 
     private static void storeIntoDatabaseIfItIsNewsPage(Document doc) {
@@ -83,14 +135,18 @@ public class Main {
     }
 
     private static boolean isInterestingLink(String link) {
-        return isIndexPage(link) || isNesPage(link);
+        return isIndexPage(link) || isNesPage(link) || !isLoginPage(link);
     }
 
     private static boolean isNesPage(String link) {
-        return link.contains("news.sina.cn");
+        return link.contains("https://news.sina.cn/") || link.contains("http://news.sina.cn/");
     }
 
     private static boolean isIndexPage(String link) {
         return link.equals("https://sina.cn");
+    }
+
+    private static boolean isLoginPage(String link) {
+        return link.contains("/signin/signin");
     }
 }
