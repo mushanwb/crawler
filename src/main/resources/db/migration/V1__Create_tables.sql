e969fdbca26fb8943e2f1d1a8d0b696050a04569
mushan <49012328+mushanwb@users.noreply.github.com>
2020-08-18 16:50:46 +0800

    引入 flyway 自动化数据库构建工具 (#3)
    使用 flyway 自动化数据库构建工具,遵从文档约定,
    在 src/main/resources/db/migration 目录下创建 sql 文件
    最后执行 mvn flyway:migrate 迁移数据库

---
+++ src/main/resources/db/migration/V1__Create_tables.sql
@@ -0,0 +1,16 @@
+create table NEWS(
+    id bigint primary key auto_increment,
+    title text,
+    content text,
+    url varchar(2000),
+    created_at timestamp,
+    modified_at timestamp
+);
+
+create table LINKS_ALREADY_PROCESSED(
+    link varchar(2000)
+);
+
+create table LINKS_TO_BE_PROCESSED(
+    link varchar(2000)
+);
