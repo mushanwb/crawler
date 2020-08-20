f0c14d96708f46fc26162d51ba36fc14e0f61cb9
mushan <49012328+mushanwb@users.noreply.github.com>
2020-08-20 16:36:54 +0800

    简单完善 MyBatisCrawlerDao 类中的方法 (#9)
    在这个提交中,我在 MyBatisCrawlerDao 类中实现了剩余的方法
    在 MyMapper.xml 文件中有实现方法需要用到的 sql 语句
    并且创建了 news 类,同时遵循 JavaBean 约定
    最后在 Crawler 类中,我将之前的 jdbc 连接更换成 mybatis

--- src/main/java/com/github/mushanwb/MyBatisCrawlerDao.java
+++ src/main/java/com/github/mushanwb/MyBatisCrawlerDao.java
@@ -8,6 +8,8 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyBatisCrawlerDao implements CrawlerDao {
     private SqlSessionFactory sqlSessionFactory;
@@ -24,13 +26,18 @@ public class MyBatisCrawlerDao implements CrawlerDao {
 
     @Override
     public boolean isLinkProcessed(String link) throws SQLException {
-        return false;
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            int count = (Integer) session.selectOne("com.github.mushanwb.MyMapper.countLink", link);
+            return count != 0;
+        }
     }
 
     @Override
     public String getNextLinkThenDelete() throws SQLException {
-        try (SqlSession session = sqlSessionFactory.openSession()) {
+        // openSession(true) 每次执行完自动提交,如果不设置 true ,默认为开启事务
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
             String link = session.selectOne("com.github.mushanwb.MyMapper.selectNextAvailableLink");
+            System.out.println(link);
             if (link != null) {
                 session.delete("com.github.mushanwb.MyMapper.deleteLink", link);
             }
@@ -40,16 +47,28 @@ public class MyBatisCrawlerDao implements CrawlerDao {
 
     @Override
     public void insertNewsIntoDatabase(String title, String content, String link) throws SQLException {
-        
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            session.insert("com.github.mushanwb.MyMapper.insertNews", new News(title, content, link));
+        }
     }
 
     @Override
     public void insertProcessedLink(String link) throws SQLException {
-
+        Map<String, Object> param = new HashMap<>();
+        param.put("tableName", "links_already_processed");
+        param.put("link", link);
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            session.insert("com.github.mushanwb.MyMapper.insertLink", param);
+        }
     }
 
     @Override
     public void insertLinkToBeProcessed(String href) throws SQLException {
-
+        Map<String, Object> param = new HashMap<>();
+        param.put("tableName", "links_to_be_processed");
+        param.put("link", href);
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            session.insert("com.github.mushanwb.MyMapper.insertLink", param);
+        }
     }
 }
---
+++ src/main/java/com/github/mushanwb/News.java
@@ -0,0 +1,46 @@
+package com.github.mushanwb;
+
+public class News {
+    private int id;
+    private String title;
+    private String content;
+    private String url;
+
+    public News(String title, String content, String url) {
+        this.title = title;
+        this.content = content;
+        this.url = url;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
